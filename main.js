b073efc0cf7058fedffb81deb852368803f6c055
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-02-21 10:11:59 -0700

    Built html form, started id labeling.

---
+++ main.js
@@ -0,0 +1,13 @@
+/*
+* JavaScript Challenge
+ */
+
+const target = getElementById('target');
+const text-color = getElementById('text-color');
+const # = getElementById('#');
+const # = getElementById('#');
+const # = getElementById('#');
+const # = getElementById('#');
+const # = getElementById('#');
+const # = getElementById('#');
+const # = getElementById('#');
