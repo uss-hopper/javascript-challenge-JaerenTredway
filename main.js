89c9f5885b5c2b5791f6dbf233fbaa7f73c75ca8
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-02-24 11:08:51 -0700

    Started adding js functions.

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
 /*
 * JavaScript Challenge
  */
-function display() {
-    alert("click me activated");
-}
- 
-function exofont() {
-        alert("exo called");    
-}
-    
- let target           = document.getElementById('target');
- let textColor        = document.getElementById('text-color');
- let exo              = document.getElementById('exo');
- let michroma         = document.getElementById('michroma');
- // const #             = document.getElementById('#');
- // const #             = document.getElementById('#');
- // const #             = document.getElementById('#');
- // const #             = document.getElementById('#');
- // const #             = document.getElementById('#');
-        
- 
- michroma.addEventListener("click", function(){target.style.color="blue";});
 
-    //michroma.onclick = alert("window onload/ michroma onclick");
+// let target           = document.getElementById('target');
+// let textColor        = document.getElementById('text-color');
+// let exo              = document.getElementById('exo');
+// let michroma         = document.getElementById('michroma');
+// const #             = document.getElementById('#');
+// const #             = document.getElementById('#');
+// const #             = document.getElementById('#');
+// const #             = document.getElementById('#');
+// const #             = document.getElementById('#');
+
+//exo.addEventListener('click', hide, false);
+
+function cursivefont() {
+   document.getElementById("target").style.fontFamily = "cursive";
+}
+
+function michromafont() {
+   document.getElementById("target").style.fontFamily = "Michroma";
+}
+
+function purplefont() {
+   document.getElementById("target").style.color = "Purple";
+}
+
+function mouseOut() {
+   document.getElementById("mouse").style.backgroundColor = "blue";
+}
+
